15337260b308edd852b59880d26e722aa7787ec4
boringmj <3239957605@qq.com>
2023-09-06 17:39:25 +0800

    Hello World!

--- index.php
+++ index.php
@@ -1,54 +1,7 @@
 <?php
 
-// copilot 帮我引入composer的autoload
 require_once __DIR__.'/vendor/autoload.php';
 
-use PhpOffice\PhpSpreadsheet\IOFactory;
-use PhpOffice\PhpSpreadsheet\Spreadsheet;
-use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
-use PhpOffice\PhpSpreadsheet\Cell\Coordinate;
+use Boringmj\Excel\Main;
 
-// 读取Excel文件
-$spreadsheet=IOFactory::load('cache/test.xlsx');
-
-// 获取第一个工作表
-$worksheet=$spreadsheet->getActiveSheet();
-
-// 获取所有数据
-$lastRow=$worksheet->getHighestRow();
-$lastColumn=$worksheet->getHighestColumn();
-print("最后一行：{$lastRow}，最后一列：{$lastColumn}\n");
-
-$lastColumnIndex=Coordinate::columnIndexFromString($lastColumn);
-$data=[];
-// 遍历单元格范围并输出值
-for ($row=1;$row<=$lastRow;$row++) {
-    for ($column=1;$column<=$lastColumnIndex;$column++) {
-        $cellAddress=Coordinate::stringFromColumnIndex($column).$row;
-        $cell=$worksheet->getCell($cellAddress);
-        echo $cell->getValue()."\n";
-        $data[$row][$column]=$cell->getValue();
-    }
-}
-
-print_r($data);
-
-// 创建一个新的电子表格对象
-$spreadsheet=new Spreadsheet();
-
-// 选择活动工作表
-$sheet=$spreadsheet->getActiveSheet();
-
-// 把data写入到表格中
-foreach ($data as $row=>$columns) {
-    foreach ($columns as $column=>$value) {
-        $cellAddress=Coordinate::stringFromColumnIndex($column).$row;
-        $sheet->setCellValue($cellAddress, $value);
-    }
-}
-
-// 保存为 Excel 文件
-$writer=new Xlsx($spreadsheet);
-$writer->save('cache/example.xlsx');
-
-echo "Excel 文件已创建成功！";
+Main::run();
---
+++ src/Main.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Boringmj\Excel;
+
+use PhpOffice\PhpSpreadsheet\IOFactory;
+use PhpOffice\PhpSpreadsheet\Spreadsheet;
+use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
+use PhpOffice\PhpSpreadsheet\Cell\Coordinate;
+
+class Main {
+    static public function run() {
+        $spreadsheet=IOFactory::load('cache/test.xlsx');
+        $worksheet=$spreadsheet->getActiveSheet();
+        $lastRow=$worksheet->getHighestRow();
+        $lastColumn=$worksheet->getHighestColumn();
+        print("最后一行：{$lastRow}，最后一列：{$lastColumn}\n");
+        $lastColumnIndex=Coordinate::columnIndexFromString($lastColumn);
+        $data=[];
+        for ($row=1;$row<=$lastRow;$row++) {
+            echo "| ";
+            for ($column=1;$column<=$lastColumnIndex;$column++) {
+                $cellAddress=Coordinate::stringFromColumnIndex($column).$row;
+                $cell=$worksheet->getCell($cellAddress);
+                $value=$cell->getValue();
+                echo "{$cellAddress} -> {$value} | ";
+                $data[$row][$column]=$value;
+            }
+            echo "\n\r";
+        }
+        $spreadsheet=new Spreadsheet();
+        $sheet=$spreadsheet->getActiveSheet();
+        foreach ($data as $row=>$columns) {
+            foreach ($columns as $column=>$value) {
+                $cellAddress=Coordinate::stringFromColumnIndex($column).$row;
+                $sheet->setCellValue($cellAddress, $value);
+            }
+        }
+        $writer=new Xlsx($spreadsheet);
+        $writer->save('cache/example.xlsx');
+        echo "Excel 文件已创建成功！";
+    }
+}
