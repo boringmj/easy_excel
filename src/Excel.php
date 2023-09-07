9819698e77ba3b5e8bdb77e41c691b4c5d2e2217
boringmj <3239957605@qq.com>
2023-09-07 15:03:33 +0800

    强迫症修改

--- src/Excel.php
+++ src/Excel.php
@@ -2,7 +2,7 @@
 
 namespace Boringmj\Excel;
 
-use  Exception;
+use Exception;
 use PhpOffice\PhpSpreadsheet\IOFactory;
 use PhpOffice\PhpSpreadsheet\Spreadsheet;
 use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
@@ -27,12 +27,11 @@ class Excel {
     static public function write(string $path,array ...$data) {
         $spreadsheet=new Spreadsheet();
         $sheet=$spreadsheet->getActiveSheet();
-        foreach ($data as $row=>$columns) {
+        foreach ($data as $row=>$columns)
             foreach ($columns as $column=>$value) {
                 $cellAddress=Coordinate::stringFromColumnIndex($column).($row+1);
                 $sheet->setCellValue($cellAddress, $value);
             }
-        }
         $writer=new Xlsx($spreadsheet);
         $writer->save($path);
     }
@@ -45,23 +44,21 @@ class Excel {
      * @throws Exception
      */
     static public function read(string $path):array {
-        if (!is_file($path)) {
+        if (!is_file($path))
             throw new Exception("{$path} 文件不存在！");
-        }
         $spreadsheet=IOFactory::load($path);
         $worksheet=$spreadsheet->getActiveSheet();
         $lastRow=$worksheet->getHighestRow();
         $lastColumn=$worksheet->getHighestColumn();
         $lastColumnIndex=Coordinate::columnIndexFromString($lastColumn);
         $data=[];
-        for ($row=1;$row<=$lastRow;$row++) {
+        for ($row=1;$row<=$lastRow;$row++)
             for ($column=1;$column<=$lastColumnIndex;$column++) {
                 $cellAddress=Coordinate::stringFromColumnIndex($column).$row;
                 $cell=$worksheet->getCell($cellAddress);
                 $value=$cell->getValue();
                 $data[$row][$column]=$value;
             }
-        }
         return $data;
     }
 }
