72f7a6b8b17620c8274342afa1aafeebe479a239
boringmj <3239957605@qq.com>
2023-09-07 16:41:06 +0800

    优化一下,完善功能

--- src/Excel.php
+++ src/Excel.php
@@ -3,6 +3,7 @@
 namespace Boringmj\Excel;
 
 use Exception;
+use PhpOffice\PhpSpreadsheet\Worksheet\Worksheet;
 use PhpOffice\PhpSpreadsheet\IOFactory;
 use PhpOffice\PhpSpreadsheet\Spreadsheet;
 use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
@@ -26,12 +27,24 @@ class Excel {
      */
     static public function write(string $path,array ...$data) {
         $spreadsheet=new Spreadsheet();
-        $sheet=$spreadsheet->getActiveSheet();
-        foreach ($data as $row=>$columns)
+        $worksheet=$spreadsheet->getActiveSheet();
+        $aligns=array();
+        foreach ($data as $row=>$columns) {
+            if(!is_array($columns))
+                continue;
             foreach ($columns as $column=>$value) {
-                $cellAddress=Coordinate::stringFromColumnIndex($column).($row+1);
-                $sheet->setCellValue($cellAddress, $value);
+                if($column<0)
+                    continue;
+                if($column===0)
+                    $aligns[$value][]=$row+1;
+                else {
+                    $cellAddress=Coordinate::stringFromColumnIndex($column).($row+1);
+                    $worksheet->setCellValue($cellAddress,$value);
+                }
             }
+        }
+        foreach ($aligns as $align=>$row)
+            self::align($worksheet,$align,...$row);
         $writer=new Xlsx($spreadsheet);
         $writer->save($path);
     }
@@ -61,4 +74,72 @@ class Excel {
             }
         return $data;
     }
+
+    /**
+     * 将指定行设置为粗体
+     * 
+     * @param string $path Excel文件路径
+     * @param int ...$row 行号
+     * @return void
+     * @throws Exception
+     */
+    static public function bold(string $path,int ...$row) {
+        if (!is_file($path))
+            throw new Exception("{$path} 文件不存在！");
+        $spreadsheet=IOFactory::load($path);
+        $worksheet=$spreadsheet->getActiveSheet();
+        foreach ($row as $r) {
+            if ($r<1)
+                continue;
+            $rowStyle=$worksheet->getStyle('A'.$r.':'.$worksheet->getHighestColumn().$r);
+            $font=$rowStyle->getFont();
+            $font->setBold(true);
+        }
+        $writer=new Xlsx($spreadsheet);
+        $writer->save($path);
+    }
+
+    /**
+     * 为指定行设置对齐方式
+     * 
+     * @param string|Worksheet $excel Excel文件路径
+     * @param string $align 对齐方式(center,left,right)
+     * @param int ...$row 行号
+     * @return void
+     * @throws Exception
+     */
+    static public function align(string|Worksheet $excel,string $align,int ...$row) {
+        if ($excel instanceof Worksheet) {
+            self::alignByObject($excel,$align,...$row);
+        } else {
+            if (!is_file($excel))
+                throw new Exception("{$excel} 文件不存在！");
+            $spreadsheet=IOFactory::load($excel);
+            $worksheet=$spreadsheet->getActiveSheet();
+            self::alignByObject($worksheet,$align,...$row);
+            $writer=new Xlsx($spreadsheet);
+            $writer->save($excel);
+        }
+    }
+
+    /**
+     * 通过对象设置对齐方式
+     * 
+     * @param Worksheet $worksheet
+     * @param string $align 对齐方式(center,left,right)
+     * @param int ...$row 行号
+     * @return void
+     * @throws Exception
+     */
+    static public function alignByObject(Worksheet $worksheet,string $align,int ...$row) {
+        if (!in_array($align,array('center','left','right')))
+            throw new Exception("{$align} 不是有效的对齐方式！");
+        foreach ($row as $r) {
+            if ($r<1)
+                continue;
+            $rowStyle=$worksheet->getStyle('A'.$r.':'.$worksheet->getHighestColumn().$r);
+            $rowStyle->getAlignment()->setHorizontal($align);
+        }
+    }
+
 }
--- src/Main.php
+++ src/Main.php
@@ -2,6 +2,7 @@
 
 namespace Boringmj\Excel;
 
+use Exception;
 use Boringmj\Excel\Excel;
 
 class Main {
@@ -10,19 +11,28 @@ class Main {
      * 运行程序
      */
     static public function run() {
-        // Excel 文件路径
-        $excel_path=dirname(__DIR__).'/cache/test.xlsx';
-        // 读取 Excel 文件
-        $data=Excel::read($excel_path);
-        // 输出数据
-        foreach ($data as $_=>$columns) {
-            foreach ($columns as $_=>$value)
-                echo $value."\t";
-            echo PHP_EOL;
+        try {
+            $excel_path=dirname(__DIR__).'/cache/temp.xlsx';
+            // 在data数组中添加表头
+            $data=array(
+                array('center','姓名','性别','年龄'),
+                array('center','张三','男','20'),
+                array('center','李四','男','21'),
+                array('center','王五','女','22')
+            );
+            Excel::write($excel_path,...$data);
+            Excel::bold($excel_path,1);
+            // 读取 Excel 文件
+            $data=Excel::read($excel_path);
+            // 输出数据
+            foreach ($data as $_=>$columns) {
+                foreach ($columns as $_=>$value)
+                    echo $value."\t";
+                echo PHP_EOL;
+            }
+        } catch (Exception $error) {
+            echo $error->getMessage();
         }
-        // 写入 Excel 文件
-        $excel_path=dirname(__DIR__).'/cache/temp.xlsx';
-        Excel::write($excel_path,...$data);
     }
 
 }
