bab74cd25569fa4b62db6161c63dba43ed1c4522
boringmj <3239957605@qq.com>
2023-09-11 09:19:52 +0800

    优化一下入口文件

--- public/index.php
+++ public/index.php
@@ -1,6 +1,9 @@
 <?php
 
-require_once __DIR__.'/../vendor/autoload.php';
+$autoload_file=__DIR__.'/../vendor/autoload.php';
+if(!file_exists($autoload_file))
+    die('Please run "composer install" first!');
+require_once $autoload_file;
 
 use Boringmj\Main;
 
